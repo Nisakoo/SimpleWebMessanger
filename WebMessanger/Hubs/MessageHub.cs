c99fa4336ceb540729cf080342aefbee241cf69b
Nisakoo <modeniki10@gmail.com>
2021-07-21 19:15:20 +0500

    Add files

---
+++ WebMessanger/Hubs/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMessanger.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public Task SendMessage(string username, string message)
+        {
+            return Clients.Others.SendAsync("ReceiveMessage", username, message);
+        }
+    }
+}
